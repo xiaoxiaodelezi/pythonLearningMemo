f3e1d606b98b497a2685f3c123c49b7519df96e5
xiaoxiaodelezi <eachdayachance@hotmail.com>
2026-07-29 23:08:51 +0800

    20260729

---
+++ opencv_mediapipe/hand_landmarker/hand_landmarker.py
@@ -0,0 +1,77 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+
+
+mp_hands = mp.tasks.vision.HandLandmarksConnections
+print(mp_hands)
+mp_drawing = mp.tasks.vision.drawing_utils
+mp_drawing_styles = mp.tasks.vision.drawing_styles
+
+MARGIN = 10
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (80, 205, 24)
+
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+    hand_landmarks_list = detection_result.hand_landmarks
+    handedness_list = detection_result.handedness
+    annotated_image = np.copy(rgb_image)
+
+    # Loop through the detected hands to visualize.
+    for idx in range(len(hand_landmarks_list)):
+        hand_landmarks = hand_landmarks_list[idx]
+        handedness = handedness_list[idx]
+
+        # Draw the hand landmarks.
+        mp_drawing.draw_landmarks(
+            annotated_image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style(),
+        )
+
+        # Get the top left corner of the detected hand's bounding box.
+        height, width, _ = annotated_image.shape
+        x_coordinates = [landmark.x for landmark in hand_landmarks]
+        y_coordinates = [landmark.y for landmark in hand_landmarks]
+        text_x = int(min(x_coordinates) * width)
+        text_y = int(min(y_coordinates) * height) - MARGIN
+
+        # Draw handedness (left or right hand) on the image.
+        cv2.putText(
+            annotated_image,
+            f"{handedness[0].category_name}",
+            (text_x, text_y),
+            cv2.FONT_HERSHEY_DUPLEX,
+            FONT_SIZE,
+            HANDEDNESS_TEXT_COLOR,
+            FONT_THICKNESS,
+            cv2.LINE_AA,
+        )
+
+    return annotated_image
+
+
+base_options = python.BaseOptions(model_asset_path="hand_landmarker.task")
+options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
+detector = vision.HandLandmarker.create_from_options(options)
+
+# img = cv2.imread("woman_hands.jpg")
+
+image = mp.Image.create_from_file("woman_hands.jpg")
+
+detection_result = detector.detect(image)
+
+# print(image.numpy_view()[:,:,:3])
+
+annotated_image = draw_landmarks_on_image(image.numpy_view()[:, :, :3], detection_result)
+cv2.imshow("women_hands",cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
